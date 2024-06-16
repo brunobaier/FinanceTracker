1911ce18033558eaa7aa8c3612c7a982d5948fac
Lucas Terêncio <lucas.tnsantos@upe.br>
2024-06-15 23:41:23 -0300

    Adicionado getters e setters do nome, senha e faixaSalarial

--- src/main/java/org/finance/entity/User.java
+++ src/main/java/org/finance/entity/User.java
@@ -2,5 +2,31 @@ package org.finance.entity;
 
 public class User {
     private String userName;
+    private String userPassword;
 
+    private float salaryRange;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public float getSalaryRange() {
+        return salaryRange;
+    }
+
+    public void setSalaryRange(float salaryRange) {
+        this.salaryRange = salaryRange;
+    }
 }
