ae1dcae2a74298785c5799739bf9fee8b5fdf547
Bruno Baier <brunobaier3@gmail.com>
2024-06-18 13:15:31 -0300

    Atualizando base de código

---
+++ src/main/java/org/finance/utils/Database.java
@@ -0,0 +1,7 @@
+package org.finance.utils;
+
+public class Database {
+    public static String jdbcUrl = "jdbc:postgresql://localhost:5432/mydatabase";
+    public static String username = "myuser";
+    public static String password = "mypassword";
+}
