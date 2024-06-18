90660279c5ecd5b1e0491cb13ba3a77c900dfb92
Bruno Baier <brunobaier3@gmail.com>
2024-06-18 20:04:22 -0300

    Modificando a classe Database

--- src/main/java/org/finance/utils/Database.java
+++ src/main/java/org/finance/utils/Database.java
@@ -22,15 +22,29 @@ public class Database {
     }
 
 
-    public void createDatabaseIfNotExist() throws SQLException{
+    public void createTables() throws SQLException{
         String sql = "CREATE TABLE IF NOT EXISTS users (" +
                 "user_id INT PRIMARY KEY AUTO_INCREMENT," +
                 "userName VARCHAR(60) NOT NULL," +
                 "lastName VARCHAR(60) NOT NULL," +
                 "password VARCHAR(100) NOT NULL," +
                 "email VARCHAR(100))";
+        String sql2 = "CREATE TABLE IF NOT EXISTS  expenses (" +
+                    "user_id INT," +
+                    "dateOfExpenses DATE," +
+                    "description VARCHAR(100)," +
+                    "FOREIGN KEY (user_id) REFERENCES user(user_id))";
+        String sql3 =
+                "user_id INT," +
+                "user_id INT," +
+                "dateOfIncomme DATE," +
+                "description VARCHAR(100)," +
+                "FOREIGN KEY (user_id) REFERENCES user(user_id))";
         try (Statement stmt = connection.createStatement()) {
             stmt.execute(sql);
+            stmt.execute(sql2);
+            stmt.execute(sql3);
+
         }
     }
 }
