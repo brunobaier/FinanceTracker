d1248c3fb9f1a933958b50aaee1d2011afd8a65d
Bruno Baier <brunobaer3@gmail.com>
2024-06-23 22:38:33 -0300

    Editando a classe Main

--- src/main/java/org/finance/app/Main.java
+++ src/main/java/org/finance/app/Main.java
@@ -1,7 +1,10 @@
 package org.finance.app;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
         
     }
 }
